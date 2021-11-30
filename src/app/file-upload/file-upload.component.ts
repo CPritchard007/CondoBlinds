a21ef7f3adea63c6592aa21e869cf7946f9d4e25
Curtis Pritchard <cpritchard649@gmail.com>
2021-11-30 16:24:14 -0500

    added file upload component

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-file-upload',
+  selector: 'file-upload',
   templateUrl: './file-upload.component.html',
   styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent implements OnInit {
 
+  fileName: string = "";
   constructor() { }
 
   ngOnInit(): void {
