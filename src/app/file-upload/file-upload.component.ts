4f9be85ca658b2df129953e73ade5f0698a8498e
Curtis Pritchard <cpritchard649@gmail.com>
2021-12-01 11:05:56 -0500

    can parse csv

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Papa } from 'ngx-papaparse';
 
 @Component({
   selector: 'file-upload',
@@ -8,9 +9,45 @@ import { Component, OnInit } from '@angular/core';
 export class FileUploadComponent implements OnInit {
 
   fileName: string = "";
+  data: Object = {};
+  dataJson: Object[] = [];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  fileSelected(event: any) {
+    let file: File = event.target.files[0];
+    if (file) {
+      this.fileName = file.name;
+      this.data = file;
+      let lines: string[][] = [];
+        
+      let reader: FileReader = new FileReader();
+      reader.readAsText(file);
+      reader.onload = (e: any) => {
+        let csv: string = reader.result as string;
+        if (csv.match(/[^\r\n]+/g) != null) {
+          
+        }
+      }
+
+      console.log(lines, "lines");
+      for (let i = 0; lines.length > i; i++) {
+        let line = lines[i];
+        console.log("to json");
+        this.dataJson.push({
+          "Group Id": line[0],
+          "Group Name": line[1],
+          "Group Type": line[2],
+          "Name": line[3],
+          "quantity": line[4],
+          "Width": line[5],
+          "Height": line[6],
+        })
+      }
+
+      console.log(this.dataJson);
+    }
+  }
 }
